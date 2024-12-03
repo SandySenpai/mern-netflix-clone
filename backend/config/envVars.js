ec79037f1cfc9212077f13f7d826715a0b75758c
Sandy <156508822+SandySenpai@users.noreply.github.com>
2024-12-03 11:23:08 +0000

    not working yet

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-	MONGO_URI: process.env.MONGO_URI,
+	MONGO_URI:"mongodb+srv://1hk21ec045:Wo5IwRCyj09P758Z@netflix.ooro0.mongodb.net/netflix?retryWrites=true&w=majority&appName=Netflix",
 	PORT: process.env.PORT || 5000,
-	JWT_SECRET: process.env.JWT_SECRET,
-	NODE_ENV: process.env.NODE_ENV,
-	TMDB_API_KEY: process.env.TMDB_API_KEY,
+	JWT_SECRET: "Hum",
+	NODE_ENV: "development",
+	TMDB_API_KEY: "",
 };
